d330484ecd0da14887c36b3f8964241a57f59646
lesguillemets <lesguillemets@users.noreply.github.com>
2017-04-16 13:09:16 +0900

    looping

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
 use std::fmt;
+use std::io;
 
 fn main() {
     let board: Board = Board::initial();
     println!("{}", board);
-    board.print_count();
-    println!("{:?}", board.place(Face::Black, (2, 3)));
-    if let Ok(b) = board.place(Face::White, (2, 3)) {
-        println!("{}", b);
+    main_loop(&board, Face::Black);
+}
+
+fn read_hand() -> (i8,i8){
+    loop {
+        if let Some(hand) = read_loc() {
+            println!("{:?}",hand);
+            return hand;
+        } else {
+            println!("invalid input");
+        }
+    }
+}
+
+fn main_loop(board:&Board, player:Face) -> (){
+    let hand = read_hand();
+    match board.place(player, hand) {
+        Ok(b) => {
+            println!("{}", b);
+            main_loop(&b, player.not())
+        }
+        Err(e) => {
+            println!("{}", e);
+            main_loop(board,player)
+        }
     }
 }
 
@@ -98,7 +120,7 @@ impl Board {
                         turn_any = true;
                         for flip in flips.iter() {
                             // FIXME JUST NOW
-                            println!("{:?}", flip);
+                            // println!("{:?}", flip);
                             b.assign(player, *flip).expect("ierror");
                         }
                     }
@@ -184,5 +206,26 @@ impl fmt::Display for Board {
             buf.push_str("\n");
         }
         write!(f, "{}", buf)
+    }
+}
+
+fn read_loc() -> Option<(i8,i8)>{
+    // TODO: only valid for SIZE < 10
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).expect("read error");
+    let mut ch = buf.chars();
+    match (ch.next(), ch.next()){
+        (Some(r), Some(c)) => from_char(r,c),
+        _ => None
+    }
+}
+
+fn from_char(r:char, c:char) -> Option<(i8,i8)> {
+    let row = r as usize;
+    let col = c as usize;
+    if row >= 97 && row < 97+SIZE && col >= 48 && col < 48 + SIZE {
+        Some((col as i8 - 48 , row as i8 -97))
+    } else {
+        None
     }
 }
