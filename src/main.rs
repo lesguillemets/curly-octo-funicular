854537927fa2a2990ba264abf1dc02b627ff96a0
lesguillemets <lesguillemets@users.noreply.github.com>
2017-04-16 13:29:46 +0900

    Rustfmt

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@ fn main() {
     main_loop(&board, Face::Black);
 }
 
-fn read_hand() -> (i8,i8){
+fn read_hand() -> (i8, i8) {
     loop {
         if let Some(hand) = read_loc() {
-            println!("{:?}",hand);
+            println!("{:?}", hand);
             return hand;
         } else {
             println!("invalid input");
@@ -18,7 +18,7 @@ fn read_hand() -> (i8,i8){
     }
 }
 
-fn main_loop(board:&Board, player:Face) -> (){
+fn main_loop(board: &Board, player: Face) -> () {
     let hand = read_hand();
     match board.place(player, hand) {
         Ok(b) => {
@@ -27,7 +27,7 @@ fn main_loop(board:&Board, player:Face) -> (){
         }
         Err(e) => {
             println!("{}", e);
-            main_loop(board,player)
+            main_loop(board, player)
         }
     }
 }
@@ -209,22 +209,22 @@ impl fmt::Display for Board {
     }
 }
 
-fn read_loc() -> Option<(i8,i8)>{
+fn read_loc() -> Option<(i8, i8)> {
     // TODO: only valid for SIZE < 10
     let mut buf = String::new();
     io::stdin().read_line(&mut buf).expect("read error");
     let mut ch = buf.chars();
-    match (ch.next(), ch.next()){
-        (Some(r), Some(c)) => from_char(r,c),
-        _ => None
+    match (ch.next(), ch.next()) {
+        (Some(r), Some(c)) => from_char(r, c),
+        _ => None,
     }
 }
 
-fn from_char(r:char, c:char) -> Option<(i8,i8)> {
+fn from_char(r: char, c: char) -> Option<(i8, i8)> {
     let row = r as usize;
     let col = c as usize;
-    if row >= 97 && row < 97+SIZE && col >= 48 && col < 48 + SIZE {
-        Some((col as i8 - 48 , row as i8 -97))
+    if row >= 97 && row < 97 + SIZE && col >= 48 && col < 48 + SIZE {
+        Some((col as i8 - 48, row as i8 - 97))
     } else {
         None
     }
